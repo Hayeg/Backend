6823c7d5b34b96d5c85f6fd62aa9a47a04020c31
Hayelom Abraha <hayeg757@gmail.com>
2024-07-07 16:34:02 -0700

    appjs

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/question", askqueastionroutes);
 app.use("/api/answer", answerquestions);
-
+app.get("/",(req, res)=>{
+	res.send("Welcome to our API");
+})
 async function start() {
 	try {
 		const [rows] = await dbconnection.execute("SELECT 'test' AS result");
